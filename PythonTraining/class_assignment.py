b28f8a908088384b538a2fb8f5eb9c3ea1de9120
venkiteshsa <venkitesh.sa@inapp.com>
2022-07-25 16:25:33 +0530

    class calculator assignment completed

--- PythonTraining/class_assignment.py
+++ PythonTraining/class_assignment.py
@@ -1,3 +1,111 @@
+from abc import ABC, abstractmethod
+class Calculate(ABC):
+    @abstractmethod
+    def calculate(self):
+        pass
+
+    @property
+    def n1(self):
+        return self.__n1
+
+    @property
+    def n2(self):
+        return self.__n2
+
+    @property
+    def ans(self):
+        return self.__ans
+
+    @n1.setter
+    def n1(self, n1):
+        self.__n1 = n1
+
+    @n2.setter
+    def n2(self, n2):
+        self.__n2 = n2
+
+    @ans.setter
+    def ans(self, ans):
+        self.__ans = ans
+
+class CalcSum(Calculate):
+    def __init__(self, n1, n2):
+        self.n1 = n1
+        self.n2 = n2
+        
+
+    def calculate(self):
+        self.ans = self.n1 + self.n2
+        return self.ans
+
+class CalcDiff(Calculate):
+    def __init__(self, n1, n2):
+        self.n1 = n1
+        self.n2 = n2
+        
+    def calculate(self):
+        self.ans = self.n1 - self.n2
+        return self.ans
+
+class CalcProd(Calculate):
+    def __init__(self, n1, n2):
+        self.n1 = n1
+        self.n2 = n2
+        
+    def calculate(self):
+        self.ans = self.n1 * self.n2
+        return self.ans
+class CalcQuo(Calculate):
+    def __init__(self, n1, n2):
+        self.n1 = n1
+        self.n2 = n2
+        
+    def calculate(self):
+        if self.n2 !=0:
+            self.ans = self.n1 // self.n2
+            return self.ans
+        else:
+            print("Divisor can't be 0")
+    
+opt = 0
+
+while opt!=5:
+
+    print("Calculator")
+    print("1. Sum \n 2. Difference \n 3. Product \n 4. Quotient \n 5. Exit")
+    opt = int(input("Enter your option:"))
+
+    if opt ==  1:
+        n1 = int(input("Enter first number:"))
+        n2 = int(input("Enter second number:"))
+        c = CalcSum(n1, n2)
+        c.calculate()
+        print(c.ans)
+    elif opt == 2:
+        n1 = int(input("Enter first number:"))
+        n2 = int(input("Enter second number:"))
+        c = CalcDiff(n1, n2)
+        c.calculate()
+        print(c.ans)
+    elif opt == 3:
+        n1 = int(input("Enter first number:"))
+        n2 = int(input("Enter second number:"))
+        c = CalcProd(n1, n2)
+        c.calculate()
+        print(c.ans)
+    elif opt == 4:
+        n1 = int(input("Enter first number:"))
+        n2 = int(input("Enter second number:"))
+        c = CalcQuo(n1, n2)
+        c.calculate()
+        print(c.ans)
+    elif opt == 5:
+        pass
+    else:
+        print("Incorrect option!")
+
+
+"""
 class Calculator:
     def __init__(self, n1, n2):
         self.n1 = n1
@@ -15,3 +123,4 @@ class Calculator:
 calculate1 = Calculator(3, 6)
 print("Sum =", calculate1.add(), "Product =", calculate1.multiply(), "Remainder =", calculate1.divide())
 
+"""
--- PythonTraining/py_filehandling.py
+++ PythonTraining/py_filehandling.py
@@ -1,4 +1,5 @@
 #File handling
+"""
 
 #trying to open a file myfile.txt in the same dir
 #using open() method get the file object
@@ -11,3 +12,157 @@ myFile = open("myfile.txt", "r")
 print(myFile.readline())
 print(myFile.readline())
 print(myFile.readline())
+
+
+
+#A simple example for higher order function
+#which can accept at least one fn and can optionally return a fn
+
+#a simple fn with a print statement
+def greet(name):
+    return "Hello {}".format(name)
+
+#defining the higher order fn which can accept fn
+def print_greetings(fn, param):
+    print(fn(param))
+
+#calling the higher order fn
+print_greetings(greet, 'Abhi')
+
+
+#map function
+#a higher order fn which can accept a fn and also a list of iterable params.
+#each param will be applied to the fn and result will be returned back as a map obj.
+#we can later convert this map obj into set/tuple
+
+
+def mymapfunction(a):
+    return a*a
+
+x = map(mymapfunction, (1,2,3,4,5))
+print(tuple(x))
+
+print(tuple(map(lambda x:x*x, (1,2,3,4,5))))
+
+#print('\n'.join(tuple(map(lambda a, p = int(input('Enter power: ')): f'{a} ^ {p} = {int(a) ** p}', input('Enter list of numbers: ').split()))))
+
+#filter function
+#filter fn filters the iterables based on condition 
+#it accepts the fn and also the iterables as params.
+
+def filterfn(x):
+    if x>=3:
+        return x
+
+y = filter(filterfn, (1,2,3,4))
+print(list(y))
+
+y = filter(lambda x:(x>=3), (1,2,3,4))
+print(list(y))
+
+
+from functools import reduce
+
+
+print(reduce(lambda a, b: a + b, filter(lambda x: 3 < x < 9, map(int, input('Enter numbers: ').split()))))
+print('Sum:', reduce(lambda a, b: a + b, filter(lambda x: 3 < x < 9, map(int, input('Enter numbers: ').split()))))
+
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @property
+    @abstractmethod
+    def diet(self):
+        pass
+
+    @property
+    def food_eaten(self):
+        return self.__food
+
+    @food_eaten.setter
+    def food_eaten(self, food):
+        if food in self.diet:
+            self.__food = food
+        else:
+            raise ValueError(f"This animal doesn't eat this food")
+
+
+    @abstractmethod
+    def feed(self):
+        pass
+ 
+    @abstractmethod
+    def do(self, action):
+        pass
+
+class Lion(Animal):
+    @property
+    def diet(self):
+        return ['antelope', 'cheetah', 'buffalo']
+    def feed(self):
+        print(f"Feeding a lion with {self.food_eaten}!")
+    def do(self, action, time):
+        print(f"{action} a lion at {time}")
+class Panda(Animal):
+    @property
+    def diet(self):
+        return ['bamboo', 'leaves']
+    def feed(self):
+        print(f"Feeding a panda with {self.food_eaten}!")
+    def do(self, action, time):
+        print(f"{action} a panda at {time}")
+
+class Snake(Animal):
+    @property
+    def diet(self):
+        return ['mice', 'rabbit']
+    def feed(self):
+        print(f"Feeding a snake with {self.food_eaten}!")
+    def do(self, action, time):
+        print(f"{action} a snake at {time}")
+
+simba = Lion()
+po = Panda()
+cobra = Snake()
+
+simba.food_eaten="buffalo"
+simba.feed()
+po.food_eaten='bamboo'
+po.feed()
+cobra.food_eaten='rice'
+cobra.feed()
+
+
+import os 
+if os.path.exists("myfile.txt"):
+    os.rename("myfile.txt", "myfilenew.txt")
+    print("The file name has been renamed")
+else:
+    print("The file does not exist")
+
+
+import os
+from unittest import result
+#creating a new directory
+#os.mkdir("mkdir")
+#print the current working directory
+#print(os.getcwd())
+#change the current working directory
+#os.chdir("mkdir")
+#print(os.getcwd())
+#delete the directory that we created
+#os.rmdir("mkdir")
+#get the list of files and folders in a dir
+#result = os.listdir(os.getcwd())
+#print(result)
+
+
+"""
+
+#run an external python file(fileoutputsave.py)
+#save its results as txt file
+
+import subprocess
+with open("myfile.txt", "wb") as f:
+    subprocess.check_call()
